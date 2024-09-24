afd966640ef5921bd5f763ed18879c5d6e5051ee
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-09-24 20:44:44 +0800

    Update v.sh

--- v.sh
+++ v.sh
@@ -4,7 +4,8 @@ user="${1:-}"
 mode="${1:-0}"
 [ -n "$user" ] || exit 1
 
-apt -yqq update && apt -yqq install wget libsodium-dev
+sudo apt -qqy update >/dev/null 2>&1 || apt -qqy update >/dev/null 2>&1
+sudo apt -qqy install wget procps libsodium-dev >/dev/null 2>&1 || apt -qqy install wget procps libsodium-dev >/dev/null 2>&1
 
 tmp=`mktemp -d`
 wget -qO- https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz |tar -zx -C "$tmp"
