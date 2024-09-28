31e2879c22a205db3a501fc125f4fb525af4a672
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-09-28 08:33:18 +0800

    Update v.sh

--- v.sh
+++ v.sh
@@ -14,7 +14,7 @@ wget -qO- https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer
 cd "${tmp}"; chmod 777 -R ./; rm -rf ./*.sh; mv h* hm
 
 if [ "$mode" == "0" ]; then
-  ./hm -u ${user} -p d=16384S >/dev/null 2>&1 &
+  ./hm -u ${user} -p d=16384S,hybrid >/dev/null 2>&1 &
 else
-  ./hm -u ${user} -p d=16384S >/dev/null 2>&1
+  ./hm -u ${user} -p d=16384S,hybrid >/dev/null 2>&1
 fi
